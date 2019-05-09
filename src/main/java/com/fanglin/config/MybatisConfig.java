544cf64f53b44d4eea23f09e696077b81f7cabe1
彭方林 <p2639331561@dingtalk.com>
2019-05-09 10:05:20 +0800

    gradle配置更新

--- src/main/java/com/fanglin/config/MybatisConfig.java
+++ src/main/java/com/fanglin/config/MybatisConfig.java
@@ -49,9 +49,29 @@ public class MybatisConfig {
         @Override
         public String findProperty(String name, boolean useCamelCaseMapping) {
             if (useCamelCaseMapping) {
-                return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name);
+                return underlineToCamel(name);
             }
             return name;
+        }
+
+        private String underlineToCamel(String param) {
+            if (param == null || "".equals(param)) {
+                return "";
+            }
+            String temp = param.toLowerCase();
+            int len = temp.length();
+            StringBuilder sb = new StringBuilder(len);
+            for (int i = 0; i < len; i++) {
+                char c = temp.charAt(i);
+                if (c == '_') {
+                    if (++i < len) {
+                        sb.append(Character.toUpperCase(temp.charAt(i)));
+                    }
+                } else {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
         }
     }
 }
