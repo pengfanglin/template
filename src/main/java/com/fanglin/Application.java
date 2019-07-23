10f64a58894c80693f9ce2d1246aca4b861811c7
彭方林 <p2639331561@dingtalk.com>
2019-07-23 10:00:23 +0800

    /

--- src/main/java/com/fanglin/Application.java
+++ src/main/java/com/fanglin/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
-
 /**
  * 应用程序启动类
  *
